ca3954951b6838c44ae03af3a5ac3af8faeeaf56
Riyabansal2017 <32256171+Riyabansal2017@users.noreply.github.com>
2019-12-01 21:47:16 +0530

    Update (58)LengthofLastWord.java

--- (58)LengthofLastWord.java
+++ (58)LengthofLastWord.java
@@ -4,11 +4,11 @@ class Solution {
     public int lengthOfLastWord(String s) {
         int count=0;
         int ref=s.length()-1;
-        while(i>=0 && s.charAt(i) == ' ')
+        while(ref>=0 && s.charAt(i) == ' ')
         {
             ref--;
         }
-        while(i>=0 && s.charAt(i) != ' ')
+        while(ref>=0 && s.charAt(i) != ' ')
         {
             count++;
             ref--;

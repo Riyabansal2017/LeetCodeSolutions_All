8ad803ea4b606e9060e3ddffeeec767f79a092a2
Riyabansal2017 <32256171+Riyabansal2017@users.noreply.github.com>
2019-12-10 22:34:01 +0530

    Reverse linked list q added

---
+++ (206)ReverseLinkedList.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution
+{
+    public ListNode reverseList(ListNode head)
+    {
+        if(head==null || head.next == null)
+        {
+          return head;
+        }
+        ListNode curr = head;
+        ListNode prev = null;
+        while(curr != null)
+        {
+          ListNode temp = curr.next;
+          curr.next = prev;
+          prev = curr;
+          curr = temp;
+        }
+      return prev;
+    }
+}
